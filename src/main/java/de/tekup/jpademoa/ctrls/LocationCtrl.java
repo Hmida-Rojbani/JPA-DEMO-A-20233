f9cd391234045939347ae86a10ee95bdacdba7f3
Hmida-Rojbani <hmida.rojbani@fst.utm.tn>
2022-10-10 16:15:33 +0100

    save location part2

---
+++ src/main/java/de/tekup/jpademoa/ctrls/LocationCtrl.java
@@ -0,0 +1,23 @@
+package de.tekup.jpademoa.ctrls;
+
+import de.tekup.jpademoa.entities.LocationEntity;
+import de.tekup.jpademoa.services.LocationService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@AllArgsConstructor
+public class LocationCtrl {
+    private LocationService locationService;
+
+    @PostMapping("/locations/add/client/{clientId}")
+    @ResponseBody
+    public LocationEntity registerLocation(@PathVariable("clientId") int clientId,
+                               @RequestBody LocationEntity location){
+        return locationService.saveLocation(location,clientId);
+    }
+}
