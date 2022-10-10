087b81774cfcf692ba8702c7e41fd8f6466408ec
Hmida-Rojbani <hmida.rojbani@fst.utm.tn>
2022-10-10 15:57:59 +0100

    save location part1

--- src/main/java/de/tekup/jpademoa/services/LocationService.java
+++ src/main/java/de/tekup/jpademoa/services/LocationService.java
@@ -1,9 +1,22 @@
 package de.tekup.jpademoa.services;
 
+import de.tekup.jpademoa.entities.ClientEntity;
+import de.tekup.jpademoa.entities.LocationEntity;
+import de.tekup.jpademoa.repos.LocationRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class LocationService {
 
+    private ClientService clientService;
+    private LocationRepository locationRepository;
     //TODO add insert Location into DB
+    public LocationEntity saveLocation(LocationEntity location,
+                                       int clientId){
+        ClientEntity client = clientService.getClientById(clientId);
+        location.setClient(client);
+        return locationRepository.save(location);
+    }
 }
