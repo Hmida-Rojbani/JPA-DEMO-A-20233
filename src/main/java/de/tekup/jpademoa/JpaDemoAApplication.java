0b1cc57b80d692f9ec5bbc41ebba55ab8e551701
Hmida-Rojbani <hmida.rojbani@fst.utm.tn>
2022-12-11 22:57:48 +0100

    create keypair with openssl to use it to sign JWT Token and add them to configuration.

--- src/main/java/de/tekup/jpademoa/JpaDemoAApplication.java
+++ src/main/java/de/tekup/jpademoa/JpaDemoAApplication.java
@@ -1,9 +1,12 @@
 package de.tekup.jpademoa;
 
+import de.tekup.jpademoa.security.config.RsaKeyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(RsaKeyProperties.class)
 public class JpaDemoAApplication {
 
     public static void main(String[] args) {
